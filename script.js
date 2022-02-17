45e1faaf5b370995fb1af9fbb906f2d1ffe4d917
agentCrafteur289 <99748322+agentCrafteur289@users.noreply.github.com>
2022-02-17 11:25:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const titreSpans = document.querySelectorAll('h1 span');
 const btns = document.querySelectorAll('.btn-first');
 const logo = document.querySelector('.logo');
 const medias = document.querySelectorAll('.bulle');
-const bullos = document.querySelector('.bullos');
 
 window.addEventListener('load', () => {
 
@@ -12,8 +11,7 @@ window.addEventListener('load', () => {
     .staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
     .staggerFrom(btns, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1')
     .from(logo, 0.4, {transform: "scale(0)", ease: "power2.out"}, '-=2')
-    .staggerFrom(medias, 1, {right: -200, ease: "power2.out"}, 0.3, '-=1')
-    .staggerFrom(bullos, 1, {right: -200, ease: "power2.out"}, 0.3, '-=1');
+    .staggerFrom(medias, 1, {right: -200, ease: "power2.out"}, 0.3, '-=1');
 
     
     
